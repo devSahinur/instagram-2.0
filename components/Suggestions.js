d80869f07d60f65e0aea576e9999a626594f180a
Sahinur Islam <infosahinur@gmail.com>
2022-01-01 03:34:15 +0600

    suggestions agein aip uri set

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -1,1 +1,19 @@
+import { useEffect, useState } from "react";
 
+function Suggestions() {
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=10")
+      .then((res) => res.json())
+      .then((data) => setSuggestions(data.results));
+  }, []);
+
+  return (
+    <div>
+      <h1>i am suggestions</h1>
+    </div>
+  );
+}
+
+export default Suggestions;
