17a2d6df0adb41ee78ccb0d7b7f51b9b6904b2e4
Sahinur Islam <infosahinur@gmail.com>
2021-12-31 11:22:43 +0600

    stories components added

--- components/Feed.js
+++ components/Feed.js
@@ -1,12 +1,14 @@
+import Stories from "./Stories";
+
 function Feed() {
   return (
-    <main>
-      <section>
-        {/* Stories */}
+    <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto ">
+      <section className="col-span-2" >
+        <Stories />
         {/* Post */}
       </section>
 
-      <section>
+      <section className="col-span-1">
         {/* Mini Profile */}
         {/* suggestions */}
       </section>
---
+++ components/Stories.js
@@ -0,0 +1,9 @@
+function Stories() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Stories
