1415e4103aa2be4250f4fe19d7488b70b4e26a9b
Sahinur Islam <infosahinur@gmail.com>
2021-12-31 11:10:33 +0600

    Feed page added

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 import Head from "next/head";
+import Feed from "../components/Feed";
 import Header from "../components/Header";
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Instagram 2.0</title>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <Header />
+      <Feed />
+
     </div>
   );
 }
