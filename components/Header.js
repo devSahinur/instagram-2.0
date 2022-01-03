6493ed4eec7889393fd164c63cf1ab95b06c13d5
Sahinur Islam <infosahinur@gmail.com>
2022-01-03 09:10:07 +0600

    header page router fix

--- components/Header.js
+++ components/Header.js
@@ -9,15 +9,17 @@ import {
 } from "@heroicons/react/outline";
 import { HomeIcon } from "@heroicons/react/solid";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 function Header() {
   const { data: session } = useSession();
-  console.log(session);
+  const router = useRouter();
+  // console.log(session);
   return (
     <div className="shadow-sm border-b bg-white sticky top-0 z-50">
       <div className="flex justify-between max-w-6xl mx-5 xl:mx-auto">
         {/* Left */}
-        <div className="relative hidden lg:inline-grid cursor-pointer w-24">
+        <div onClick={() => router.push('/')} className="relative hidden lg:inline-grid cursor-pointer w-24">
           <Image
             src="https://links.papareact.com/ocw"
             layout="fill"
@@ -26,7 +28,7 @@ function Header() {
           />
         </div>
 
-        <div className="relative w-10 lg:hidden flex-shrink-0 cursor-pointer">
+        <div onClick={() => router.push('/')} className="relative w-10 lg:hidden flex-shrink-0 cursor-pointer">
           <Image
             src="https://links.papareact.com/jjm"
             layout="fill"
@@ -49,7 +51,7 @@ function Header() {
 
         {/* Right */}
         <div className="flex items-center justify-end space-x-4">
-          <HomeIcon className="navBtn" />
+          <HomeIcon onClick={() => router.push('/')} className="navBtn" />
           <MenuIcon className="h-6 md:hidden cursor-pointer" />
 
           {session ? (
