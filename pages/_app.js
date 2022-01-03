2f1e550782fb590d03aa102cdce57676c863b67a
Sahinur Islam <infosahinur@gmail.com>
2022-01-03 09:12:21 +0600

    _app js page recoil set

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import { SessionProvider } from "next-auth/react";
+import { RecoilRoot } from "recoil";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
     </SessionProvider>
   );
 }
