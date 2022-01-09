5cf29caf6d705521a3733b9d17d86eda35ddb114
Sahinur Islam <infosahinur@gmail.com>
2022-01-09 12:02:13 +0600

    comment impliment

--- components/Post.js
+++ components/Post.js
@@ -6,9 +6,49 @@ import {
   HeartIcon,
   PaperAirplaneIcon,
 } from "@heroicons/react/outline";
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  serverTimestamp,
+} from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { db } from "../firebase";
 // import { BookmarkIcon } from "@heroicons/react/solid";
 
 function Post({ post }) {
+  const { data: session } = useSession();
+  const [comment, setComment] = useState("");
+  const [comments, setComments] = useState([]);
+  const id = post.id;
+
+  useEffect(() => {
+    onSnapshot(
+      query(
+        collection(db, "posts", id, "comments"),
+        orderBy("timestamp", "desc")
+      ),
+      (snapshot) => setComments(snapshot.docs)
+    );
+  }, [db]);
+
+  const sendComment = async (e) => {
+    e.preventDefault();
+
+    const commentToSend = comment;
+    setComment("");
+
+    await addDoc(collection(db, "posts", id, "comments"), {
+      comment: commentToSend,
+      username: session.user.username,
+      userImage: session.user.image,
+      timestamp: serverTimestamp(),
+    });
+  };
+
   return (
     <div className="bg-white my-7 border rounded-sm">
       {/* Header */}
@@ -31,30 +71,63 @@ function Post({ post }) {
       />
 
       {/* Button */}
-      <div className="flex justify-between px-4">
-        <div className="flex space-x-4 ">
-          <HeartIcon className="btn" />
-          <ChatIcon className="btn" />
-          <PaperAirplaneIcon className="btn" />
+      {session && (
+        <div className="flex justify-between px-4">
+          <div className="flex space-x-4 ">
+            <HeartIcon className="btn" />
+            <ChatIcon className="btn" />
+            <PaperAirplaneIcon className="btn" />
+          </div>
+          <BookmarkIcon className="btn" />
         </div>
-        <BookmarkIcon className="btn" />
-      </div>
+      )}
 
       {/* caption */}
       <p className="p-5 truncate">
-          <span className="font-bold mr-1">{post.data().username} </span>
-          {post.data().caption}
+        <span className="font-bold mr-1">{post.data().username} </span>
+        {post.data().caption}
       </p>
 
       {/* comments */}
+      {comments.length > 0 && (
+        <div className="ml-10 h-20 overflow-y-scroll scrollbar-thumb-black scrollbar-thin">
+          {comments.map((comment) => (
+            <div className="flex items-center space-x-2 mb-3" key={comment.id}>
+              <img
+                className="h-7 rounded-full"
+                src={comment.data().userImage}
+                alt=""
+              />
+              <p className="text-sm flex-1">
+                <span className="font-bold">{comment.data().username} </span>{" "}
+                {comment.data().comment}
+              </p>
+            </div>
+          ))}
+        </div>
+      )}
 
       {/* input box */}
-
-      <form className="flex items-center p-4">
+      {session && (
+        <form className="flex items-center p-4">
           <EmojiHappyIcon className="h-7" />
-          <input type="text" placeholder="Add a comments..." className="border-none flex-1 focus:ring-0 outline-none" />
-          <button className="font-semibold text-blue-400">Post</button>
-      </form>
+          <input
+            type="text"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+            placeholder="Add a comments..."
+            className="border-none flex-1 focus:ring-0 outline-none"
+          />
+          <button
+            type="submit"
+            onClick={sendComment}
+            disabled={!comment.trim()}
+            className="font-semibold text-blue-400"
+          >
+            Post
+          </button>
+        </form>
+      )}
     </div>
   );
 }
